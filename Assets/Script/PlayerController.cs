6effab656a5143fe0622c9dcfc5795f5176073bb
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-06-03 15:05:35 -0300

    implementacion de movimiento basico

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,16 +12,14 @@ public class PlayerController : MonoBehaviour
     float running;
     float mHorizontal;
     float mVertical;
-    public CharacterController controller;
+    [SerializeField] UnityEngine.CharacterController controller; // Utiliza UnityEngine.CharacterController
     float mGravity = -9.81f;
     Vector3 playerVelocity;
-
 
     [SerializeField] float maxTiltAngle = 45f; // Ángulo máximo de inclinación hacia adelante
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
     }
 
@@ -28,28 +27,29 @@ public class PlayerController : MonoBehaviour
     {
         UpdatePosition();
         GetAxisInput();
+    }
+
+    void GetAxisInput()
+    {
+        mHorizontal = Input.GetAxisRaw("Horizontal");
+        mVertical = Input.GetAxisRaw("Vertical");
+        playerVelocity.x = mHorizontal * walkSpeed;
+        playerVelocity.z = mVertical * walkSpeed;
+    }
+
+    void UpdatePosition()
+    {
+        Vector3 move = new Vector3(mHorizontal, playerVelocity.y, mVertical);
+        move = this.transform.TransformDirection(move);
+        controller.Move(move * Time.deltaTime * walkSpeed);
+    }
+}
+
+
         //bool isGrounded = Physics.Raycast(transform.position, Vector3.down, controller.height / 2f + 0.1f);
-        // playerVelocity.x = mHorizontal * walkSpeed;
-        // playerVelocity.z = mVertical * walkSpeed;
 
         // // Inclinar hacia adelante
         // float tiltAngle = Mathf.Clamp(playerVelocity.magnitude * maxTiltAngle, -maxTiltAngle, maxTiltAngle);
         // transform.rotation = Quaternion.Euler(tiltAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
 
         //controller.Move(playerVelocity * Time.deltaTime);
-    }
-
-    void GetAxisInput()
-    {
-        mHorizontal = Input.GetAxis("Horizontal");
-        mVertical = Input.GetAxis("Vertical");
-
-    }
-    void UpdatePosition()
-    {
-        Vector3 move = new Vector3(mHorizontal,playerVelocity.y,mVertical);
-        move = this.transform.TransformDirection(move);
-        walking = walkSpeed;
-        controller.Move(move * Time.deltaTime * walking );
-    }
-}
