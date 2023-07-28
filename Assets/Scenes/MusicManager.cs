60dbc3a486d52f7b5036ee1c9f6c86bbe0c6105c
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-07-28 12:25:53 -0300

    minimapa

--- Assets/Scenes/MusicManager.cs
+++ Assets/Scenes/MusicManager.cs
@@ -7,23 +7,33 @@ public class MusicManager : MonoBehaviour
 
     private void Start()
     {
+        // Asegurarse de que el personaje tenga un AudioSource (agregarlo si no existe).
         audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Zona1"))
         {
-            Debug.Log("holaaaaaaaaa");
             PlayMusic(0); // Índice 0 para la música de la Zona1.
         }
-        else if (other.CompareTag("Zona2"))
+        else if (other.CompareTag("ExitCollider"))
         {
             PlayMusic(1); // Índice 1 para la música de la Zona2.
         }
         // Agrega más comparaciones y cambios de música para cada zona adicional.
     }
-
+private void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("Zona1")){
+    audioSource.Stop();
+    }
+    
+}
     private void PlayMusic(int index)
     {
         if (index >= 0 && index < zoneMusic.Length)
